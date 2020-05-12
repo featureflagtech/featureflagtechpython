02cee583bee92647eedbc53baa049fec7876b253
CharlieHodgkinson <charlotte.hodgkinson4@gmail.com>
2020-05-12 15:19:14 +0100

    add get flag method

--- featureflagtech/main.py
+++ featureflagtech/main.py
@@ -1,11 +1,15 @@
 import requests 
 
 class FeatureFlag:
-    def __init__(self, key):
+    def __init__(self, opts):
         self.URL = "https://api.featureflag.tech"
-        self.apikey = {"fft-api-key": key}
-        # self.apikey = {"fft-api-key": "38312f60-557a-11ea-8dfc-5976faa419b4"}
+        self.apikey = opts  # = {"fft-api-key": "38312f60-557a-11ea-8dfc-5976faa419b4"}
+        self.flags = {}
 
-    def get(self):
+    def getFlag(self):
         r = requests.get(url = self.URL, headers = self.apikey)
-        print(r.json())
+        self.flags = r.json()
+        print(self.flags)
+
+    def get(self, flagName):
+        return self.flags.get(flagName)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name = 'featureflagtech',
-  version = '0.0.2',
+  version = '0.0.3',
   author = 'Charlie Hodgkinson',
   author_email = 'charlotte.hodgkinson4@gmail.com',
   description = 'devlopment of python featureflag client',
