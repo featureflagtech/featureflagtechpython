04c0470ef99a95a77b08597e24be6885004fd16b
CharlieHodgkinson <charlotte.hodgkinson4@gmail.com>
2020-05-19 16:09:54 +0100

    workflow

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
+setup(
   name = 'featureflagtech',
   version = '0.0.3',
   author = 'Charlie Hodgkinson',
   author_email = 'charlotte.hodgkinson4@gmail.com',
   description = 'devlopment of python featureflag client',
-  long_description = long_description,
+  long_description = open('README.md').read(),
   long_description_content_type = 'text/markdown',
   url = 'https://github.com/featureflagtech/featureflagtechpython',
-  packages = setuptools.find_packages(),
+  packages = ['featureflagtech'],
+  install_requires=[
+    'requests'
+  ],
   classifiers = [
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: Apache Software License",
